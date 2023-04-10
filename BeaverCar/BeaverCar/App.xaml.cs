8acc94b8391043f1d8668eee39f4a1ea4bf511a4
CryFish <CryFish@CRYFISH-PC>
2023-04-10 11:14:06 +0300

    Добавьте файлы проекта.

---
+++ BeaverCar/BeaverCar/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BeaverCar.Services;
+using BeaverCar.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeaverCar
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BeaverCar/BeaverCar/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BeaverCar.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BeaverCar.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
