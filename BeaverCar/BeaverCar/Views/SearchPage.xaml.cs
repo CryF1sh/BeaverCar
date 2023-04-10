51fbbac270160df7077757129c532b6f45bf05cd
20IS21 <20is21@vpmt.ru>
2023-04-10 22:38:20 +0300

    Создана страница SearchPage, которая отвечает за просмотр карты

---
+++ BeaverCar/BeaverCar/Views/SearchPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+//using Xamarin.Forms.Maps;
+
+namespace BeaverCar.Views
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+            //Map map = new Map();
+            //Content = map;
+        }
+    }
+}
