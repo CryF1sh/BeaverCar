5d8281baa80a80a05b72739dee3ee0f15c86378b
Alexandr <miskaskormom@gmail.com>
2023-04-11 21:59:45 +0300

    Тут нихера не сделанный вход (Смс говна поел реально,) Рупасов

--- BeaverCar/BeaverCar/Views/pageEnter.xaml.cs
+++ BeaverCar/BeaverCar/Views/pageEnter.xaml.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Newtonsoft.Json;
-using System.Net;
+using System.Net.Http;
+
 using BeaverCar.Class;
 
 namespace BeaverCar.Views
@@ -16,34 +17,35 @@ namespace BeaverCar.Views
     public partial class pageEnter : ContentPage
     {
         List<User> listUsers;
+        HttpClient client;
         public pageEnter()
         {
-            InitializeComponent();
-            var client = new WebClient();
-            var responce = client.DownloadString("http://192.168.43.65:65226/api/Users");
-            listUsers = JsonConvert.DeserializeObject<List<User>>(responce);
+            InitializeComponent();            
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(contrPhone.Text))
-            {
-                DisplayAlert("Ошибка", "Номер не был введён", "Ok");
-                return;
-            }
-            try
-            {
-                var sotr = listUsers.FirstOrDefault(p => p.PhoneNumber == contrPhone.Text);
-                if (sotr == null)
-                    DisplayAlert("Ошибка", "Такого номера нет", "Ок");
-                else
-                    DisplayAlert("1", "Т2", "Ок");
-                    //Navigation.PushAsync();
-            }
-            catch
-            {
-                DisplayAlert("Ошибка", "Введён неправельный номер", "Ок");
-            }
+            client = new HttpClient();
+            //var responce = client.DownloadString("http://192.168.43.65:65226/api/Users");
+            //listUsers = JsonConvert.DeserializeObject<List<User>>(responce);
+            //if (string.IsNullOrEmpty(contrPhone.Text))
+            //{
+            //    DisplayAlert("Ошибка", "Номер не был введён", "Ok");
+            //    return;
+            //}
+            //try
+            //{
+            //    var sotr = listUsers.FirstOrDefault(p => p.PhoneNumber == contrPhone.Text);
+            //    if (sotr == null)
+            //        DisplayAlert("Ошибка", "Такого номера нет", "Ок");
+            //    else
+            //        DisplayAlert("1", "Т2", "Ок");
+                    Navigation.PushAsync(new pageToken(client));
+            //}
+            //catch
+            //{
+            //    DisplayAlert("Ошибка", "Введён неправельный номер", "Ок");
+            //}
         }
     }
 }
---
+++ BeaverCar/BeaverCar/Views/pageToken.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeaverCar.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pageToken : ContentPage
+    {
+        public pageToken(HttpClient client)
+        {
+            InitializeComponent();
+        }
+    }
+}
